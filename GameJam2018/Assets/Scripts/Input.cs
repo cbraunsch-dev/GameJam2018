731af643ca7bd96113cddf6c49c921d860966f49
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2018-01-26 20:46:32 +0100

    #4 - Control second player using controller

---
+++ GameJam2018/Assets/Scripts/Input.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input : MonoBehaviour {
+	private Rigidbody2D rb;
+	public int movementForce = 300;
+	public int groundHorizontalAcceleration = 10;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var controls = gameObject.GetComponent<Player_ControllerAdapter> ();
+		float groundMovement = controls.HorizontalMovement;
+		var acceleration = groundHorizontalAcceleration;
+		Vector2 horizontalForce = new Vector2 (groundMovement * acceleration, 0.0f);
+
+		rb.velocity = horizontalForce;
+	}
+}
