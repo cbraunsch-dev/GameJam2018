f15a13966925e0263026ebbc92e9af23340b13b4
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2018-01-26 19:25:26 +0100

    #1 - Initial commit

---
+++ GameJam2018/Assets/Input.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input : MonoBehaviour {
+	private Rigidbody2D rb;
+	public int movementForce = 300;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce (Vector2.left * this.movementForce);
+	}
+}
