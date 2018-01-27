26ad4d4f35effc95361309f83fa6cff3593f106c
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2018-01-27 15:53:42 +0100

    #13 - Randomly spawn cats in predefined spawn locations

---
+++ GameJam2018/Assets/Scripts/GameManager_Input.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_Input : MonoBehaviour {
+	public int numberOfCatsAliveAtATime = 3;
+	public float delayBeforeSpawningNewCat = 3f;	//In seconds
+	public GameObject whiteCat;
+	public List<GameObject> spawnPoints = new List<GameObject>();
+	private List<GameObject> cats = new List<GameObject>();
+	private float timeUntilSpawnNewCat;
+
+	// Use this for initialization
+	void Start () {
+		timeUntilSpawnNewCat = delayBeforeSpawningNewCat;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cats.Count < numberOfCatsAliveAtATime) {
+			timeUntilSpawnNewCat -= Time.deltaTime;
+			if (timeUntilSpawnNewCat <= 0) {
+				//Spawn new cat
+				var newCat = Instantiate (whiteCat);
+				var indexOfRandomSpawnPoint = Random.Range(0, spawnPoints.Count);
+				var spawnPoint = spawnPoints [indexOfRandomSpawnPoint];
+				newCat.transform.position = spawnPoint.transform.position;
+				timeUntilSpawnNewCat = delayBeforeSpawningNewCat;
+				this.cats.Add (newCat);
+			}
+		} else {
+			timeUntilSpawnNewCat = delayBeforeSpawningNewCat;
+		}
+	}
+}
