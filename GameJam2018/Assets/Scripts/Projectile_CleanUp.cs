e4ae4d5ec1a9a59c13fb6add588484a6111bfb3e
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2018-01-27 09:55:00 +0100

    #6 - Clean up projectiles after a few seconds

---
+++ GameJam2018/Assets/Scripts/Projectile_CleanUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile_CleanUp : MonoBehaviour {
+
+	public float timeToLive;
+	private float remainingTimeToLive;
+
+	// Use this for initialization
+	void Start () {
+		remainingTimeToLive = timeToLive;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		remainingTimeToLive -= Time.deltaTime;
+		if (remainingTimeToLive <= 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision2D collision) {
+		Destroy (gameObject);
+	}
+}
