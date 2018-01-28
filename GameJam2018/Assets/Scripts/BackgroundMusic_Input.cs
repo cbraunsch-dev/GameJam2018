a3c1e43c716a15defd399789abcc6371a3f76837
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2018-01-28 14:29:10 +0100

    #57 - Add music

---
+++ GameJam2018/Assets/Scripts/BackgroundMusic_Input.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic_Input : MonoBehaviour {
+
+	private static BackgroundMusic_Input instance = null;
+	public static BackgroundMusic_Input Instance {
+		get { return instance; }
+	}
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		var audioSource = GetComponent<AudioSource> ();
+		audioSource.Play ();
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
+
+
