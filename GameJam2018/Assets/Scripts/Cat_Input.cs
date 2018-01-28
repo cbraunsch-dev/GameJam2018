a015c902a0c340e5407f49f489fc8e1cd75952b0
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2018-01-28 11:39:08 +0100

    #20 - Animate cat walkinig

--- GameJam2018/Assets/Scripts/Cat_Input.cs
+++ GameJam2018/Assets/Scripts/Cat_Input.cs
@@ -1,21 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Cat_Input : MonoBehaviour {
 	private int health = 5;
+	private Rigidbody2D rb;
 	public float Damage { get; private set; } //The damage the cat inflicts on the Roomba upon contact
 
 	// Use this for initialization
 	void Start () {
 		this.Damage = 0.2f;
+		this.rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		GetComponent<Animator> ().SetFloat ("Speed", GetComponent<AIPath>().velocity.magnitude);
 	}
-
 
 	void OnTriggerEnter2D(Collider2D collider) {
 		if (collider.tag == Tags.AttackingParticle) {
---
+++ GameJam2018/Assets/Scripts/Triggers.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triggers : MonoBehaviour {
+	public const string CatWalk = "Walk";
+}
