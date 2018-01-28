02d1d712435530da730651341011c4b2f1b203b5
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2018-01-28 13:42:32 +0100

    #56 - Add ability to show green overlay while charing but keep it disables for now

--- GameJam2018/Assets/Scripts/Energy_Input.cs
+++ GameJam2018/Assets/Scripts/Energy_Input.cs
@@ -5,7 +5,11 @@ using UnityEngine.UI;
 
 public class Energy_Input : MonoBehaviour {
 	private int maxEnergy = 20;
+	private float flashSpeed = 5f;
+	private Color flashColor = new Color (0f, 1f, 0f, 0.25f);
+	private bool charged = false;
 	public Slider energySlider;
+	public Image energyImage;
 
 	public int Energy {
 		get;
@@ -19,10 +23,23 @@ public class Energy_Input : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//IndicateCharge ();	
+	}
+
+	void IndicateCharge ()
+	{
+		if (energyImage != null) {
+			if (charged) {
+				energyImage.color = flashColor;
+			} else {
+				energyImage.color = Color.Lerp (energyImage.color, Color.clear, flashSpeed * Time.deltaTime);
+			}
+		}
+		charged = false;
 	}
 
 	public void AddEnergy(int energy) {
+		this.charged = true;
 		this.Energy += energy;
 		if (this.Energy > this.maxEnergy) {
 			this.Energy = this.maxEnergy;
