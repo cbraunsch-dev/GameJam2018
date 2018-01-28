7ea82ae6229c701bac7f4b1939066d57288d408d
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2018-01-28 14:50:19 +0100

    #54 - Add cat sounds

--- GameJam2018/Assets/Scripts/GameManager_Input.cs
+++ GameJam2018/Assets/Scripts/GameManager_Input.cs
@@ -11,18 +11,25 @@ public class GameManager_Input : MonoBehaviour {
 	public GameObject tigerCat;
 	public List<GameObject> spawnPoints = new List<GameObject>();
 	public Text numberOfCatsKilledText;
+	public List<AudioClip> catAudioClips = new List<AudioClip> ();
 	private List<GameObject> cats = new List<GameObject>();
+	private AudioSource catAudioSource;
 	private float timeUntilSpawnNewCat;
 	private int numberOfCatsKilled = 0;
+	private bool catsAround = false;
+	private float timeSinceLastCatSound = 0;
+	private float timeBetweenCatSounds = 4;
 
 	// Use this for initialization
 	void Start () {
 		timeUntilSpawnNewCat = delayBeforeSpawningNewCat;
 		this.UpdateKillCountText ();
+		catAudioSource = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		PlayCatSounds ();
 		if (cats.Count < numberOfCatsAliveAtATime) {
 			timeUntilSpawnNewCat -= Time.deltaTime;
 			if (timeUntilSpawnNewCat <= 0) {
@@ -39,8 +46,28 @@ public class GameManager_Input : MonoBehaviour {
 		}
 	}
 
+	private void PlayCatSounds() {
+		if (catsAround) {
+			timeSinceLastCatSound += Time.deltaTime;
+			if (timeSinceLastCatSound >= timeBetweenCatSounds) {
+				timeSinceLastCatSound = 0;
+				PlayRandomCatSound ();
+			}
+		} else {
+			timeSinceLastCatSound = 0;
+		}
+	}
+
+	private void PlayRandomCatSound() {
+		var audioClipIndex = Random.Range (0, catAudioClips.Count);
+		catAudioSource.clip = this.catAudioClips [audioClipIndex];
+		catAudioSource.Play ();
+	}
+
 	private GameObject SpawnCat() {
+		catsAround = true;
 		var cat = Instantiate (tigerCat);
+		catsAround = true;
 		return cat;
 	}
 
